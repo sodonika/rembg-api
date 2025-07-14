c455485bc964c4a7fe3672b9174f580952ab8f1c
sodonika <116317757+sodonika@users.noreply.github.com>
2025-07-15 02:06:32 +0300

    Initial commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, send_file
 from rembg import remove
 from io import BytesIO
 from PIL import Image
-
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -22,3 +22,9 @@ def remove_bg():
     output_image.save(byte_io, 'PNG')
     byte_io.seek(0)
     return send_file(byte_io, mimetype='image/png')
+
+    
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
